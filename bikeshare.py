576881cf03d72cc4fe7fe2a25415d29d0472fed1
noura-udacity-project <ankush.garg0201@gmail.com>
2020-12-19 15:39:31 +0530

    refactor: Changed docstring for a function in the bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -11,7 +11,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month and day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
